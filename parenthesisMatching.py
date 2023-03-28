bb7f68608d772ac68f9a4c09f487f09801415151
Zhu-HH <zhh1274@gmail.com>
2023-03-28 10:46:21 +0800

    修复部分括号匹配失败问题

--- parenthesisMatching.py
+++ parenthesisMatching.py
@@ -3,37 +3,50 @@
 """
 from Python_Stack import Stack
 stack = Stack()
+
 str_dic = {
     "(":")",
     "[":"]",
     "{":"}",
 }
+# 左括号与右括号
 str_left = ["(","{","["]
 str_right = [")","}","]"]
+
 def parenthesisMatching(str_data):
+    """
+    输入字符串,判断括号是否匹配
+    :param str_data: {([()()()((((}()))))])}
+    :return: True
+    """
     for index,strs in enumerate(str_data):
         if strs in str_left:
             stack.push(strs)
         else:
-            if strs in str_right and not stack.isEmpty():
+            if strs in str_right and not stack.isEmpty():   # 此判断 有效解决了只出现一个右括号的问题
                 pop_data = stack.pop()
                 if str_dic[pop_data] != strs:
                     return False
                 else:
                     continue
             else:
-                if stack.isEmpty() and index == len(str_data)-1:
+                if stack.isEmpty() and (strs not in str_right) and index == len(str_data)-1:
+                    return True
+                elif strs in str_right:
                     return False
-                else:
-                    continue
     else:
         if stack.isEmpty():
             return True
         else:
             return False
-print(parenthesisMatching('{([()()()((((}()))))])}'))
-# print(parenthesisMatching('(1+1)-(){{}{}}'))
-# print(parenthesisMatching('(1+1)-(1+1)*{{1+1}-{1+1}}'))
+
 # print(parenthesisMatching(']'))
-# print(parenthesisMatching('[()0}(){}]'))
+# print(parenthesisMatching('['))
+# print(parenthesisMatching('[(])'))
+print(parenthesisMatching('{([()()()((((()))))])}'))
+# print(parenthesisMatching('[(1)(2){3*3}]'))
+print(parenthesisMatching('(1+1)-(){{}{}}'))
+print(parenthesisMatching('(1+1)-(1+1)*{{1+1}-{1+1}}+1'))
+
+
 
